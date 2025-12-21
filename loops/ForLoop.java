7c6e4bccb17d2b502c683e089d97561188b2c778
abir1807 <146002209+abir1807@users.noreply.github.com>
2025-12-21 18:20:26 +0530

    Add for loop example

---
+++ loops/ForLoop.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class ForLoopDemo {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+    }
+}
