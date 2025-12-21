e720062b2e6f4327507819e15a6a80b389b353bf
abir1807 <146002209+abir1807@users.noreply.github.com>
2025-12-21 17:54:04 +0530

    Add EvenOdd program using conditional statements

---
+++ conditions/EvenOdd.java
@@ -0,0 +1,14 @@
+// Program to check whether a number is even or odd
+
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
